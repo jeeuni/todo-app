d6c9074bd132f58569f2fa6d61776725cf0ca073
jeeuni <cutee1600@naver.com>
2024-03-06 15:24:26 +0900

    class3

---
+++ demo.js
@@ -0,0 +1,34 @@
+// 객체->
+// me = {
+//   name: "lee",
+//   gender: "male",
+//   age: 43
+// }
+
+// you = {
+//   name: "kim",
+//   gender: "female",
+//   age: 37
+//   address: null;
+// }
+// me.age;
+
+//함수
+function sum(x, y) {
+  result = x + y;
+  return result;
+}
+console.log(you.name);
+console.log(you.age);
+console.log(you.sum(20, 20));
+function hi(name) {
+  console.log("hi~ " + name);
+}
+
+nm = "lee";
+
+hi(nm);
+
+abc = sum(200, 200);
+
+console.log("abc", abc);
